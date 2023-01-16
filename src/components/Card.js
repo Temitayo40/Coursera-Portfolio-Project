5dac435c5f1c2bfd37670ba7befa56a9c0b76da0
Adebowale <adebowaletemitayo40@gmail.com>
2023-01-16 22:42:41 +0100

    coursera advance react project

---
+++ src/components/Card.js
@@ -0,0 +1,42 @@
+import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const Card = ({ title, description, imageSrc }) => {
+  // Implement the UI for the Card component according to the instructions.
+  // You should be able to implement the component with the elements imported above.
+  // Feel free to import other UI components from Chakra UI if you wish to.
+  return (
+    <VStack spacing={4} backgroundColor="white" borderRadius={10}>
+      <Image src={imageSrc} borderRadius={10} w="100%"/>
+      <VStack spacing={4} padding={4}>
+        <Heading size='md' textTransform='uppercase' alignSelf="flex-start"
+        color="black">
+          {title}
+        </Heading>
+        <Text color="gray.800">{description}</Text>
+        <HStack w="100%" justifyContent="flex-start">
+          <Text
+            fontSize='sm'
+            color="black"
+            alignSelf="flex-start"
+            fontWeight="bold"
+          >
+            See More
+          </Text>
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            size="1x"
+            color="black"
+            style={{
+              marginTop: 3,
+            }}
+          />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
+};
+
+export default Card;
---
+++ src/components/ContactMeSection.js
@@ -0,0 +1,124 @@
+import React, {useEffect} from "react";
+import { useFormik, yupToFormErrors } from "formik";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import * as Yup from 'yup';
+import FullScreenSection from "./FullScreenSection";
+import useSubmit from "../hooks/useSubmit";
+import {useAlertContext} from "../context/alertContext";
+
+const formSchema = Yup.object({
+   firstName: Yup.string()
+     .required('Name is required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Email is required'),
+  type: Yup.string()
+    .required('Type is required'),
+  comment: Yup.string()
+    .required('Comment is required')
+ });
+
+const LandingSection = () => {
+  const {isLoading, response, submit} = useSubmit();
+  const { onOpen } = useAlertContext();
+
+  const { getFieldProps, errors, touched, handleSubmit, dirty } = useFormik({
+    initialValues: {
+      firstName: '',
+      email: '',
+      type: 'hireMe',
+      comment: '',
+    },
+    onSubmit: (values) => {
+      submit('https://example.com', values);
+    },
+    validationSchema: formSchema,
+  });
+
+  useEffect(() => {
+    if (response) {
+      onOpen(response.type, response.message);
+    }
+  }, [response]);
+
+  return (
+    <FullScreenSection
+      isDarkBackground
+      backgroundColor="#512DA8"
+      py={16}
+      spacing={8}
+    >
+      <VStack w="1024px" p={32} alignItems="flex-start">
+        <Heading as="h1" id="contactme-section">
+          Contact me
+        </Heading>
+        <Box p={6} rounded="md" w="100%">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit(e);
+            }}
+          >
+            <VStack spacing={4}>
+              <FormControl isInvalid={!!errors.firstName && touched.firstName}>
+                <FormLabel htmlFor="firstName">Name</FormLabel>
+                <Input
+                  id="firstName"
+                  name="firstName"
+                  {...getFieldProps('firstName')}
+                />
+                <FormErrorMessage>{errors.firstName}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!!errors.email && touched.email}>
+                <FormLabel htmlFor="email">Email Address</FormLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  {...getFieldProps('email')}
+                />
+                <FormErrorMessage></FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                <Select id="type" name="type" {...getFieldProps('type')}>
+                  <option value="hireMe">Freelance project proposal</option>
+                  <option value="openSource">
+                    Open source consultancy session
+                  </option>
+                  <option value="other">Other</option>
+                </Select>
+              </FormControl>
+              <FormControl isInvalid={!!errors.comment && touched.comment}>
+                <FormLabel htmlFor="comment">Your message</FormLabel>
+                <Textarea
+                  id="comment"
+                  name="comment"
+                  height={250}
+                  {...getFieldProps('comment')}
+                />
+                <FormErrorMessage></FormErrorMessage>
+              </FormControl>
+              <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </VStack>
+    </FullScreenSection>
+  );
+};
+
+export default LandingSection;
